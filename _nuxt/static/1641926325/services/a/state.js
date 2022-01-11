608007be6bf2daa424113fa6ffccee6ab2b69cf7
mbabaei0 <mbabaei0@users.noreply.github.com>
2022-01-11 18:38:46 +0000

    deploy: 8ab4b75a76bb6d9e39425591551fd17121b29015

---
+++ _nuxt/static/1641926325/services/a/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641926325",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002Fa",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39ca7d63"}}}}(null));
