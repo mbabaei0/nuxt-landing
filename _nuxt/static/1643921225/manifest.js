a5cb1f7778aeba4a4c1857c166df87bd60c8a06c
mbabaei0 <mbabaei0@users.noreply.github.com>
2022-02-10 21:05:58 +0000

    deploy: a25c77cb7d398ffafe4b1bffa0b01b50babfe091

--- _nuxt/static/1643921225/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog","\u002Forder","\u002Fservices\u002Fall","\u002Fservices\u002Fsheet","\u002Fservices\u002Fportfolio","\u002Fservices\u002Fresume","\u002Fservices\u002Fssic-sima","\u002Fservices\u002Fguiljann","\u002Fservices\u002Fc"]})
