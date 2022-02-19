d8d344821b91c6f1705b0ccaeaed80dbe93df90a
mbabaei0 <mbabaei0@users.noreply.github.com>
2022-02-19 22:36:55 +0000

    deploy: 019d0876a69f7cdf8a6228bb0a8f5b6e9c2455f9

--- _nuxt/static/1644527157/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fblog","\u002Fcontact","\u002Forder","\u002Fservices\u002Fall","\u002Fportfolio\u002Fall","\u002Fservices\u002Fsheet","\u002Fservices\u002Fportfolio","\u002Fservices\u002Fresume","\u002Fservices\u002Fssic-sima","\u002Fservices\u002Fguiljann","\u002Fservices\u002Fc"]})
--- _nuxt/static/1644527157/services/sheet/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-landing\u002F_nuxt\u002Fstatic\u002F1644527157",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002Fsheet",config:{_app:{basePath:"\u002Fnuxt-landing\u002F",assetsPath:"\u002Fnuxt-landing\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8a2cc1c9"}}}}(null));
