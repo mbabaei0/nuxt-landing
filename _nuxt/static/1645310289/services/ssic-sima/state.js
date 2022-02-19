71b7dc9cbd797a122ced63617a0c07578d0910d0
mbabaei0 <mbabaei0@users.noreply.github.com>
2022-02-19 22:38:10 +0000

    deploy: 1ea7a08f6efcffc7f1bef5c982446c19a1bfc31d

---
+++ _nuxt/static/1645310289/services/ssic-sima/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-landing\u002F_nuxt\u002Fstatic\u002F1645310289",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002Fssic-sima",config:{_app:{basePath:"\u002Fnuxt-landing\u002F",assetsPath:"\u002Fnuxt-landing\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4d6446a8"}}}}(null));
