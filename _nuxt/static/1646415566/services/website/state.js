f76cdb8fa07494df8fac32b670cddf899867f7ee
mbabaei0 <mbabaei0@users.noreply.github.com>
2022-04-20 22:11:03 +0000

    deploy: 713d5d6cb88a270e799d2bbcbb85bf19904aef6b

--- _nuxt/static/1646415566/services/website/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-landing\u002F_nuxt\u002Fstatic\u002F1646415566",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002Fwebsite",config:{_app:{basePath:"\u002Fnuxt-landing\u002F",assetsPath:"\u002Fnuxt-landing\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1c357797"}}}}(null));
---
+++ _nuxt/static/1650492662/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-landing\u002F_nuxt\u002Fstatic\u002F1650492662",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fnuxt-landing\u002F",assetsPath:"\u002Fnuxt-landing\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ccf4613f"}}}}(null));
