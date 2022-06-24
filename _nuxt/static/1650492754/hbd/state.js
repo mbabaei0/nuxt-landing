1a5a3eba160fb96cf9d0bcc5efc2312101d84d17
mbabaei0 <mbabaei0@users.noreply.github.com>
2022-06-24 18:06:45 +0000

    deploy: d23dc70b77a56f58c0d55e5beacc1cd45628f2bc

--- _nuxt/static/1650492754/hbd/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-landing\u002F_nuxt\u002Fstatic\u002F1650492754",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhbd",config:{_app:{basePath:"\u002Fnuxt-landing\u002F",assetsPath:"\u002Fnuxt-landing\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cdd2bace"}}}}(null));
